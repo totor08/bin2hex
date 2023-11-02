315f56cafc23439953d2cfb98411c294465033d5
thierry Escola <thierry.escola@imaugis.com>
2023-11-02 21:41:40 +0100

    changes to files

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 ## Convert binary file into Intel HEX format file.
 
-usage: bin2hex [-h] [-l [launchAddr]] [-s [locationAddr]] [-w WIDTH]
-               filename
+usage: bin2hex [-h] [-l [LOADADDRESS]] [-s [STARTADDRESS]] [-w WIDTH] filename
 
 Convert a binary file into Intel HEX format
 
@@ -10,13 +9,25 @@ positional arguments:
 
 optional arguments:
   -h, --help            show this help message and exit
-  -l [LAUNCHADDRESS], --launchAddress [LAUNCHADDRESS]
-                        Launch Address in ABCD hex format
+  -l [LOADADDRESS], --loadAddress [LOADADDRESS]
+                        Load Address in ABCD hex format
   -s [STARTADDRESS], --startAddress [STARTADDRESS]
-                        Mapping Address in ABCD hex format
+                        Start Address in ABCD hex format
   -w WIDTH, --width WIDTH
                         Bytes per line
 
---launchAddress option set the tail record launch address ( refer to Intel HEX format )
---startAddrees option set the address where the binary file is supposed to be loaded.
---width option set the byte count per line, 16 by default.
+--s set the tail record launch address ( refer to Intel HEX format ), 0000 by default
+--l set the address where the binary file is supposed to be loaded, 0000 by default
+--w set the decimal byte count per line, 16 by default.
+
+example :
+bin2hex.py t1.bin -l 1234 -s 5678 -w 6
+:061234003132333435367F
+:06123A0037383930313273
+:041240003334350A04
+:0056780131
+
+bin2hex t1.bin
+:100000003132333435363738393031323334350ADA
+:00000001FF
+
--- bin2hex.py
+++ bin2hex.py
@@ -8,19 +8,25 @@ parser = argparse.ArgumentParser(
         prog='bin2hex',
         description='Convert a binary file into Intel HEX format')
 parser.add_argument("filename",help="binary filename")
-#parser.add_argument("-o","--outfile", help="nom de fichier de sortie")
-parser.add_argument("-l","--launchAddress",help="Launch Address in ABCD hex format", nargs='?', default="0")
-parser.add_argument("-s","--startAddress", help="Mapping Address in ABCD hex format", nargs='?', default="0")
+parser.add_argument("-l","--loadAddress",help="Load Address in ABCD hex format", nargs='?', default="0")
+parser.add_argument("-s","--startAddress", help="Start Address in ABCD hex format", nargs='?', default="0")
 parser.add_argument("-w","--width", help="Bytes per line", default=16, type=int)
 args = parser.parse_args()
-filename = args.filename
-# print file size
-size = os.stat(filename).st_size
-width = args.width
-print("file size {} bytes".format(size))
 
+# get filename
+filename = args.filename
+
+# get file size
+size = os.stat(filename).st_size
+
+# get width command line option
+width = args.width
+
+# get load address
 start = int(args.startAddress,16)
-launch = int(args.launchAddress,16)
+
+# get start address
+load = int(args.loadAddress,16)
 
 f = open(filename,"rb")
 while size > 0 :
@@ -28,8 +34,8 @@ while size > 0 :
         width = size
     size -= width
     buf=f.read(width)
-    bl=[width, int(start/256) & 255, start & 255, 0]
-    start += width
+    bl=[width, int(load/256) & 255, load & 255, 0]
+    load += width
     for i in range(0,width) :
         bl.append(buf[i])
     chk=0
@@ -40,7 +46,7 @@ while size > 0 :
     chk = (-chk) & 255
     print("{:02X}".format(chk))
 
-bl=[0,int(launch/256)&255, launch & 255, 1 ]
+bl=[0,int(start/256)&255, start & 255, 1 ]
 print(":",end="")
 chk=0
 for i in bl :
