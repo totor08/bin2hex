b179330b6dd3b7e358cb89544d601e84839e8750
thierry Escola <thierry.escola@imaugis.com>
2023-11-05 15:50:05 +0100

    add comments to py file

--- bin2hex.py
+++ bin2hex.py
@@ -27,25 +27,41 @@ start = int(args.startAddress,16)
 
 # get start address
 load = int(args.loadAddress,16)
-
+# open the file in read binary mode
 f = open(filename,"rb")
 while size > 0 :
+    # if remaining bytes count is less than the width
+    #  set the width
     if width > size :
         width = size
+    # decrement remaining count size by width
     size -= width
+    # read the file by width bytes
     buf=f.read(width)
+    # set a list with width, address in big indian format,
+    #  and the 0 marker
     bl=[width, int(load/256) & 255, load & 255, 0]
+    # increment the address by width
     load += width
+    # append the list with the bytes just read
     for i in range(0,width) :
         bl.append(buf[i])
+    # initialize the checksum
     chk=0
+    # print the leading ":" with no carriage return
     print(":",end="")
+    # for each element of the list
     for i in bl :
+        # compute the checksum
         chk += i
+        # print the byte in 2 hexadecimal format, with no carriage return
         print("{:02X}".format(i), end="")
+    # keep the right byte of the checksum
     chk = (-chk) & 255
+    # print the checksum in a 2 hexadecimal format, with a line feed
     print("{:02X}".format(chk))
 
+# print the start address record the same way
 bl=[0,int(start/256)&255, start & 255, 1 ]
 print(":",end="")
 chk=0
@@ -55,4 +71,5 @@ for i in bl :
 chk = (-chk) & 255
 print("{:02X}".format(chk))
 
+# close the input file
 f.close
